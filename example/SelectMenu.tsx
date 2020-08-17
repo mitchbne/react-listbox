327ca682dbee5cec69d523eff86036742541f53c
Mitch Smith <mitchpsmith1998@gmail.com>
2020-08-17 20:41:44 +1000

    create a new example

--- example/SelectMenu.tsx
+++ example/SelectMenu.tsx
@@ -30,7 +30,7 @@ export const SelectMenu = (): React.ReactNode => {
           </ListboxLabel>
           <ListboxButton className="w-full focus:outline-none">
             {({ isFocused }) => (
-              <span className={`rounded px-3 py-2 border w-full text-left inline-flex items-center justify-between ${isFocused || isOpen ? "border-blue-300 shadow-outline": "border-gray-300"}`}>
+              <span className={`rounded px-3 py-2 border w-full text-left inline-flex items-center bg-white justify-between ${isFocused || isOpen ? "border-blue-300 shadow-outline": "border-gray-300"}`}>
                 { selectedWrestler ? selectedWrestler : "Select a wrestler" }
                 <svg className="h-5 w-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
                   <path clipRule="evenodd" d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" fillRule="evenodd" />
@@ -39,7 +39,7 @@ export const SelectMenu = (): React.ReactNode => {
             )}
           </ListboxButton>
           {isOpen && (
-            <ListboxList className="w-full mt-2 absolute focus:outline-none max-h-56 border shadow rounded-md overflow-y-auto">
+            <ListboxList className="w-full mt-2 absolute focus:outline-none max-h-56 border shadow rounded-md overflow-y-auto bg-white z-40">
               { wrestlers.map(wrestler => (
                 <ListboxOption className="curor-default select-none" key={wrestler} value={wrestler}>
                   {({ isActive, isSelected }) => (
--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,16 @@
 import "react-app-polyfill/ie11"
 import * as React from "react"
 import * as ReactDOM from "react-dom"
+import { AlternativeSelectMenu } from "./AlternativeSelectMenu"
 import { SelectMenu } from "./SelectMenu"
 
 const App = (): React.ReactNode => (
-  <div className="bg-white min-h-screen w-full antialiased py-20">
-    <div className="mx-auto max-w-xs">
-      <SelectMenu />
+  <div className="bg-cool-gray-100 min-h-screen w-full antialiased py-20">
+    <div className="mx-auto max-w-xs ">
+      <div className="w-full flex flex-col space-y-4">
+        <SelectMenu />
+        <AlternativeSelectMenu />
+      </div>
     </div>
   </div>
 )
