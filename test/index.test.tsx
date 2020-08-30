bb75b1e9a5d08461182ef0474883dd6a73217c85
Sam Dawson <sam.dawson181@gmail.com>
2020-08-30 17:04:47 +1000

    Add tests for Listbox

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,20 +1,54 @@
 import * as React from "react"
-import * as ReactDOM from "react-dom"
-import { Listbox, ListboxButton, ListboxLabel, ListboxList, ListboxOption } from "../src"
+import userEvent from "@testing-library/user-event"
+import { render, screen } from "@testing-library/react"
+import {
+  Listbox,
+  ListboxButton,
+  ListboxLabel,
+  ListboxList,
+  ListboxOption,
+} from "../src"
 
-describe("all components render", () => {
-  it("renders without crashing", () => {
-    const div = document.createElement("div")
-    ReactDOM.render(
-      <Listbox onChange={() => null} value="selectedVal">
-        <ListboxLabel>Select something</ListboxLabel>
-        <ListboxButton>Click me</ListboxButton>
-        <ListboxList>
-          <ListboxOption value="item1">Item 1</ListboxOption>
-          <ListboxOption value="item2">Item 2</ListboxOption>
-          <ListboxOption value="item3">Item 3</ListboxOption>
-        </ListboxList>
-      </Listbox>, div)
-    ReactDOM.unmountComponentAtNode(div)
+describe("given listbox is determined by isOpen prop", () => {
+  const onChange = jest.fn()
+  const optionValues = ["item1", "item2", "item3"]
+  
+  beforeEach(() => {
+    render(
+      <Listbox onChange={onChange} value="selectedVal">
+        {({ isOpen }: { isOpen: boolean }) => (
+          <>
+            <ListboxLabel>Select something</ListboxLabel>
+            <ListboxButton>Click me</ListboxButton>
+            {isOpen && <ListboxList>
+              <ListboxOption value={optionValues[0]}>Item 1</ListboxOption>
+              <ListboxOption value={optionValues[1]}>Item 2</ListboxOption>
+              <ListboxOption value={optionValues[2]}>Item 3</ListboxOption>
+            </ListboxList>}
+          </>
+        )}
+      </Listbox>
+    )
   })
+
+  describe("when clicking on the button", () => {
+    beforeEach(() => {
+      screen.getByRole("button").click()
+    })
+
+    it("then displays the listbox", () => {
+      const listbox = screen.getByRole("listbox")
+
+      expect(listbox).toBeInTheDocument()
+    })
+
+    describe("when clicking on an option", () => {
+      it("then calls onChange", () => {
+        const [option] = screen.getAllByRole("option")
+        userEvent.click(option)
+        
+        expect(onChange).toHaveBeenCalledWith(optionValues[0])
+      })
+    })
+  })  
 })
